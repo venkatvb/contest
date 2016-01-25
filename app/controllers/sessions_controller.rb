e9e399eba850c00f8002ec7e70c708e9c77c9619
venkatvb <venkateshbabu95@gmail.com>
2016-01-25 09:46:54 +0530

    already signed in added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   	if account and account.authenticate(params[:session][:password])
   		sign_in account
   		flash[:success] = "Login success!"
-      redirect_to '/'
+      redirect_to contests_index_path
   	else
   		flash[:danger] = "Oh snap! Invalid username or password"
   		render 'new'
@@ -12,11 +12,15 @@ class SessionsController < ApplicationController
   end
 
   def new
+    if signed_in?
+      flash[:danger] = "You were already signed in!"
+      redirect_to contests_index_path
+    end
   	@current_account = nil
   end
 
   def destroy
   	sign_out
-  	redirect_to '/'
+  	redirect_to contests_index_path
   end
 end
