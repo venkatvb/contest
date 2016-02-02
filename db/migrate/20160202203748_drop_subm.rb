7123adb574f7f1a0ab63c696ff9a4229ad320cf1
venkatvb <venkateshbabu95@gmail.com>
2016-02-03 02:19:29 +0530

    cascading delete

--- db/migrate/20160202203748_drop_subm.rb
+++
@@ -1,6 +0,0 @@
-class DropSubm < ActiveRecord::Migration
-  def change
-  	drop_table :submissions
-  	drop_table :accounts
-  end
-end
--- db/migrate/20160202204729_drop_tables.rb
+++ db/migrate/20160202204729_drop_tables.rb
@@ -1,6 +1,5 @@
 class DropTables < ActiveRecord::Migration
   def change
-  	drop_table :submissions
-  	drop_table :accounts
+  	drop_table :accounts, force: :cascade
   end
 end
